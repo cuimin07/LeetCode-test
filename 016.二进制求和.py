1540b8f3a11e3da21da823a62282999fc1e6c2cf
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2019-10-20 19:50:14 +0800

    算法题-二进制求和

---
+++ 016.二进制求和.py
@@ -0,0 +1,34 @@
+'''
+给定两个二进制字符串，返回他们的和（用二进制表示）。
+输入为非空字符串且只包含数字 1 和 0。
+
+示例 1:
+
+输入: a = "11", b = "1"
+输出: "100"
+
+示例 2:
+
+输入: a = "1010", b = "1011"
+输出: "10101"
+'''
+
+#答：
+#【内置函数】
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a, 2) + int(b, 2))[2:]
+        
+#【非内置函数】
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        r, p = '', 0
+        d = len(b) - len(a)
+        a = '0' * d + a
+        b = '0' * -d + b
+        for i, j in zip(a[::-1], b[::-1]):
+            s = int(i) + int(j) + p
+            r = str(s % 2) + r
+            p = s // 2
+        return '1' + r if p else r
+
