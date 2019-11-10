cee8b764a7d89db7f1f9a952a226a9576f7903cc
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2019-11-10 11:30:50 +0800

    算法题-回文链表

---
+++ 057.回文链表.py
@@ -0,0 +1,41 @@
+'''
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+'''
+
+
+#答：
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast = slow = ListNode(0)
+        fast = slow = head
+        stack = []
+        while fast and fast.next:
+            stack.append(slow.val)
+            slow = slow.next
+            fast = fast.next.next
+        if fast:
+            slow = slow.next
+        while slow:
+            top = stack.pop()
+            if top != slow.val:
+                return False
+            slow = slow.next
+        return True
+    
