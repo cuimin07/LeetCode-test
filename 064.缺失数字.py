729a652ca9e648c786f2cc1325d1c2c3bc0dcc39
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2019-11-13 10:40:28 +0800

    算法题-缺失数字

---
+++ 064.缺失数字.py
@@ -0,0 +1,32 @@
+'''
+给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+
+示例 1:
+输入: [3,0,1]
+输出: 2
+
+示例 2:
+输入: [9,6,4,2,3,5,7,0,1]
+输出: 8
+
+说明:
+你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+'''
+
+#答：
+#【希哈表】
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
+                
+#【位运算】
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^=  i ^ num
+        return missing
