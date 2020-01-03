65f8ed5a904bb7db4156f692d539cdd9d9464e0f
cuimin07 <48578114+cuimin07@users.noreply.github.com>
2020-01-03 17:08:27 +0800

    算法题-字母大小写全排列

---
+++ 168.字母大小写全排列.py
@@ -0,0 +1,34 @@
+'''
+给定一个字符串S，通过将字符串S中的每个字母转变大小写，我们可以获得一个新的字符串。返回所有可能得到的字符串集合。
+
+示例:
+输入: S = "a1b2"
+输出: ["a1b2", "a1B2", "A1b2", "A1B2"]
+
+输入: S = "3z4"
+输出: ["3z4", "3Z4"]
+
+输入: S = "12345"
+输出: ["12345"]
+
+注意：
+S 的长度不超过12。
+S 仅由数字和字母组成。
+'''
+
+#答：【递归】
+class Solution:
+    def letterCasePermutation(self, S: str) -> List[str]:
+        ans = [[]]
+        for char in S:
+            n = len(ans)
+            if char.isalpha():
+                for i in range(n):
+                    ans.append(ans[i][:])
+                    ans[i].append(char.lower())
+                    ans[n+i].append(char.upper())
+            else:
+                for i in range(n):
+                    ans[i].append(char)
+        return map(''.join, ans)
+        
